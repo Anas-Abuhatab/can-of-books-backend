660d63ba8d3ac06f08fa0ba8c6ea55f61b5e3a36
Anas-Abuhatab <anasm.abuhatab@gmail.com>
2021-09-24 17:04:16 +0300

    get,delete,post2

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -3,7 +3,6 @@
 
 const { bookModle } = require("../modules/book.Model");
 
-
 const getBookController = (req, res) => {
     bookModle.find().then(data => {
         res.status(200).json(data)
@@ -11,40 +10,35 @@ const getBookController = (req, res) => {
 }
 
 const createBookController = (req, res) => {
-
     let dataBook = req.body;
     let newBook = new bookModle(dataBook)
-   
     newBook.save()
     bookModle.find().then(data => {
-        res.status(200).json(data)
+        res.status(201).json(data)
     });
-    
-    
 }
 
 const deleteBookController = (req, res) => {
     let bookID = req.params.id;
-    bookModle.findByIdAndDelete({bookID}).then(() => {
-    bookModle.find().then(data => res.json(data))
-    })
-
+    bookModle.findByIdAndDelete(bookID).then(() => {
+        bookModle.find().then(data => {
+            res.status(200).json(data)
+        });
+    });
 }
 
-const updateBookController = async(req,res)=>{
+const updateBookController = async (req, res) => {
     let bookID = req.params.id;
     let updatedBook = req.body;
-    bookModle.findOne({_id:bookID}).then(data =>{
-        data.title=updatedBook.title;
-        data.description=updatedBook.description;
-        data.email=updatedBook.email;
-        data.status=updatedBook.status;
+    bookModle.findOne({ _id: bookID }).then(data => {
+        data.title = updatedBook.title;
+        data.description = updatedBook.description;
+        data.email = updatedBook.email;
+        data.status = updatedBook.status;
         data.save();
     });
-       let booklist =await bookModle.find({});
-       res.status(200).send(booklist)
-    
-    
+    let booklist = await bookModle.find({});
+    res.status(200).send(booklist);
 };
 
 module.exports = {
@@ -52,4 +46,4 @@ module.exports = {
     createBookController,
     deleteBookController,
     updateBookController
-}
+};
